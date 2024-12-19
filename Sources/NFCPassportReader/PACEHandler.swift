e99079453fac7a4167e17c4893e1f59819694c4a
Mika Hautz <mika.hautz@mohemian.com>
2024-12-19 09:26:59 +0100

    Undo formatting

--- Sources/NFCPassportReader/PACEHandler.swift
+++ Sources/NFCPassportReader/PACEHandler.swift
@@ -61,7 +61,7 @@ public class PACEHandler {
     private var cipherAlg : String = ""
     private var digestAlg : String = ""
     private var keyLength : Int = -1
-
+    
     public init(cardAccess : CardAccess, tagReader: TagReader, updateMessageHandler : ((NFCViewDisplayMessage) -> Void)? = nil) throws {
         self.tagReader = tagReader
         
